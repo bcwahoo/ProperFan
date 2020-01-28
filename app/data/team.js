076b528384fd7a1bc6eae86e46e26fe4e472cae8
bcwahoo <bcwahoo@gmail.com>
2020-01-28 11:40:38 -0500

    Edits

---
+++ app/data/team.js
@@ -0,0 +1,205 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var teamArray = [
+    {
+      team: "Philadelphia Eagles",
+      code:"PHI",
+      logo:".\img\teams\phi.gif",
+      score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        team: "Baltimore Ravens",
+        code:"BAL",
+        logo:".\img\teams\bal.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Dallas Cowboys",
+        code:"DAL",
+        logo:".\img\teams\dal.gif",
+        score:[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      },
+      {
+        team: "Arizona Cardinals",
+        code:"ARI",
+        logo:".\img\teams\ari.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Detroit Lions",
+        code:"DET",
+        logo:"img/teams/det.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Miami Dolphins",
+        code:"MIA",
+        logo:".\img\teams\mia.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Cleveland Browns",
+        code:"CLE",
+        logo:".\img\teams\cle.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "New Orleans Saints",
+        code:"NOS",
+        logo:".\img\teams\nos.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Jacksonville Jaguars",
+        code:"JAX",
+        logo:".\img\teams\jax.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "San Francisco 49ers",
+        code:"SAN",
+        logo:".\img\teams\san.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "New England Patriots",
+        code:"NEP",
+        logo:".\img\teams\nep.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Tampa Bay Buccaneers",
+        code:"TBB",
+        logo:".\img\teams\tbb.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Los Angeles Chargers",
+        code:"LAC",
+        logo:".\img\teams\jax.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Houston Texans",
+        code:"HOU",
+        logo:".\img\teams\hou.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Carolina Panthers",
+        code:"CAR",
+        logo:".\img\teams\car.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Tennessee Titans",
+        code:"TEN",
+        logo:".\img\teams\ten.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "New York Jets",
+        code:"NYJ",
+        logo:".\img\teams\nyj.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Minnesota Vikings",
+        code:"MIN",
+        logo:".\img\teams\min.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Denver Broncos",
+        code:"DEN",
+        logo:".\img\teams\ten.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Pittsburgh Steelers",
+        code:"PIT",
+        logo:".\img\teams\pit.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+      {
+        team: "Indianapolis Colts",
+        code:"IND",
+        logo:".\img\teams\ind.gif",
+        score:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      },
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = teamArray;
+  
